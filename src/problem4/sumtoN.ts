26d5c7c00f485230a2f4e69b28173c25d2ee3526
TDN <duynguyenqwert@gmail.com>
2024-07-15 22:25:54 +0700

    problem 4: completed

---
+++ src/problem4/sumtoN.ts
@@ -0,0 +1,53 @@
+/**
+ * Tổng các số nguyên từ 1 đến N (input: n (any INT) ; type: number; output: number)
+ */
+
+/**
+ * Dùng FOR
+ *
+ * @param {number} n
+ * @returns {number}
+ *
+ * Độ phức tạp thời gian: O(n) - Duyệt qua tất cả các số từ 1 đến n.
+ * Độ phức tạp không gian: O(1) - Chỉ sử dụng một lượng không gian cố định.
+ */
+function sum_to_n_a(n: number): number {
+  let sum = 0;
+  for (let i = 1; i <= n; i++) {
+    sum += i;
+  }
+  return sum;
+}
+
+/**
+ * Dùng MATH
+ *
+ * @param {number} n
+ * @returns {number}
+ *
+ * Độ phức tạp thời gian: O(1) - Tính trực tiếp
+ * Độ phức tạp không gian: O(1) - Chỉ sử dụng một lượng không gian cố định.
+ */
+function sum_to_n_b(n: number): number {
+  return (n * (n + 1)) / 2;
+}
+
+/**
+ * Dùng RECURSIVE
+ *
+ * @param {number} n
+ * @returns {number}
+ *
+ * Độ phức tạp thời gian: O(n) - Đệ quy n lần.
+ * Độ phức tạp không gian: O(n) - Ngăn xếp gọi đệ quy độ sâu n.
+ */
+function sum_to_n_c(n: number): number {
+  if (n <= 1) {
+    return n;
+  }
+  return n + sum_to_n_c(n - 1);
+}
+
+console.log("[FOR] - Sum to 10: ", sum_to_n_a(10));
+console.log("[MATH] - Sum to 10: ", sum_to_n_b(10));
+console.log("[RECURSIVE] - Sum to 10: ", sum_to_n_c(10));
